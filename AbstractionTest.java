01f61510bb1b72d11ffce628e2baabbdb214b899
kalaiadi10 <114941188+kalaiadi10@users.noreply.github.com>
2023-03-02 05:35:05 -0800

    Add files via upload

---
+++ AbstractionTest.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class AbstractionTest {
+
+	public static void main(String[] args) 
+	{
+		AbstractionData a = new AbstractionData(1,"kalai","mumbai",10000.00);
+		a.mailcheck();
+
+
+
+	}
+
+}
